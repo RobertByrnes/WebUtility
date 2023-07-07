92d4fd2aaff38158359a264183a66dc0545692e6
RobertByrnes <robbyrnes@hotmail.co.uk>
2023-07-07 21:17:28 +0100

    refactor: bring inline with working SSLClient

--- src/https.hpp
+++ src/https.hpp
@@ -56,278 +56,295 @@ typedef vector<Header> Headers;
 
 template <class ModemDriver, class HttpClientDriver>
 class HTTPS {
-    public:
-    const static int _httpsTimeout = 300000L; // 5 minutes
-    HTTPS() {}
-    ~HTTPS() {}
+public:
+  const static int _httpsTimeout = 300000L; // 5 minutes
+  HTTPS() {}
+  ~HTTPS() {}
     
-    /**
-     * @brief Use the existing connection to send a GET request
-     * 
-     * @param simModem 
-     * @param httpClient 
-     * @param resource 
-     * 
-     * @throws int HTTPS_NONE_200_RESP = 1
-     */
-    string get(ModemDriver &simModem, HttpClientDriver &httpClient, const char * resource) {
-        if (simModem.isGprsConnected()) {
-            log_i("Making GET request");
-            httpClient.get(resource);
-            int statusCode = httpClient.responseStatusCode();
-            log_i("Status code: %i", statusCode);
-            if (statusCode == 200) {
-                std::string responseBody = std::string(httpClient.responseBody().c_str());
-                log_i("Response: %s", responseBody.c_str());
-                return responseBody;
-            } else {
-                throw 1;
-            }
-        } else {
-            throw 0;
+  /**
+   * @brief Use the existing connection to send a GET request.
+   * 
+   * @param simModem 
+   * @param httpClient 
+   * @param resource 
+   * @return string
+   * @throws int HTTPS_NONE_200_RESP = 1
+   */
+  string get(ModemDriver &simModem, HttpClientDriver &httpClient, const char * resource) {
+    if (simModem.isGprsConnected()) {
+      log_d("Making GET request");
+      httpClient.get(resource);
+      int statusCode = httpClient.responseStatusCode();
+      log_d("Status code: %i", statusCode);
+
+      if (statusCode == 200) {
+        std::string responseBody = std::string(httpClient.responseBody().c_str());
+        log_d("Response: %s", responseBody.c_str());
+        return responseBody;
+      } else {
+        throw 1;
+      }
+
+    } else {
+      throw 0;
+    }
+  }
+
+  /**
+   * @brief Send JSON in the POST request body, optionally the request
+   * may be sent using OAUTH2 Authorization headers.
+   * 
+   * @param simModem 
+   * @param httpClient 
+   * @param endPoint 
+   * @param requestBody 
+   * @param currentToken 
+   * @throws HTTPS_NONE_200_RESP
+   * @return std::string 
+   */
+  string postJSON(
+    ModemDriver &simModem, 
+    HttpClientDriver &httpClient, 
+    const char * endPoint, 
+    string requestBody, 
+    string currentToken=string("")
+  ) {
+    try {
+      if (simModem.isGprsConnected()) {
+        httpClient.setHttpResponseTimeout(HTTPS::_httpsTimeout);
+        httpClient.connectionKeepAlive();
+        httpClient.beginRequest();
+        httpClient.post(endPoint);
+        httpClient.sendHeader(HTTPS_HEADER_CONTENT_TYPE, HTTPS_JSON_HEADER);
+        httpClient.sendHeader(HTTPS_HEADER_CONTENT_LENGTH, requestBody.length());
+
+        if (!currentToken.empty()) {
+          string authHeader = "Bearer " + currentToken;
+          httpClient.sendHeader(HTTPS_OAUTH_HEADER, authHeader.c_str());
+        }  
+
+        httpClient.endRequest();
+        httpClient.write((uint8_t*)requestBody.c_str(), requestBody.length());
+
+        // Before fetching the response status code, check if client connected successfully
+        if(!httpClient.connected()) {
+          throw std::runtime_error("HTTP client not connected!");
         }
-    }
 
-    /**
-     * @brief Send JSON in the POST request body, optionally the request
-     * may be sent using OAUTH2 Authorization headers.
-     * 
-     * @param simModem 
-     * @param httpClient 
-     * @param endPoint 
-     * @param requestBody 
-     * @param currentToken 
-     * @throws HTTPS_NONE_200_RESP
-     * @return std::string 
-     */
-    string postJSON(
-        ModemDriver &simModem, 
-        HttpClientDriver &httpClient, 
-        const char * endPoint, 
-        string requestBody, 
-        string currentToken=string("")
-    ) {
-        if (simModem.isGprsConnected()) {
-            httpClient.setHttpResponseTimeout(HTTPS::_httpsTimeout);
-            httpClient.connectionKeepAlive();
-            httpClient.beginRequest();
-            httpClient.post(endPoint);
-            httpClient.sendHeader(HTTPS_HEADER_CONTENT_TYPE, HTTPS_JSON_HEADER);
-            httpClient.sendHeader(HTTPS_HEADER_CONTENT_LENGTH, requestBody.length());
-            if (!currentToken.empty()) {
-                string authHeader = "Bearer " + currentToken;
-                httpClient.sendHeader(HTTPS_OAUTH_HEADER, authHeader.c_str());
-            }  
-            httpClient.endRequest();
-            httpClient.write((uint8_t*)requestBody.c_str(), requestBody.length());
-            int statusCode = httpClient.responseStatusCode();
-            log_i("Status code: %i", statusCode);
-            if (this->responseOK(statusCode)) {
-                this->readHeaders(httpClient);
-                if (httpClient.isResponseChunked()) {
-                    log_d("The response is chunked");
-                }
-                string responseBody = httpClient.responseBody().c_str();
-                log_i("Response: %s", responseBody.c_str());
-                log_d("Body length is: %i", responseBody.length());
-                if (responseBody.length() > 0) {
-                    return responseBody;
-                } else {
-                    return "";
-                }
-            }
-            throw 1;
+        int statusCode = httpClient.responseStatusCode();
+        log_d("Status code: %i", statusCode);
+
+        if (this->responseOK(statusCode)) {
+          this->readHeaders(httpClient);
+
+          if (httpClient.isResponseChunked()) {
+            log_d("The response is chunked");
+          }
+
+          string responseBody = httpClient.responseBody().c_str();
+          log_d("Response: %s", responseBody.c_str());
+          log_d("Body length is: %i", responseBody.length());
+
+          if (responseBody.length() > 0) {
+            return responseBody;
+          }
         } else {
-            throw 0;
-        } 
-    }
+          throw std::runtime_error("Non-200 response received!");
+        }
+      } else {
+        throw std::runtime_error("GPRS not connected!");
+      }
+    } catch (const std::runtime_error& e) {
+      log_e("Exception in postJSON: %s", e.what());
+    } 
+    return "";
+  }
 
-    /**
-     * @brief A simple API for sending a manually contructed request.
-     * 
-     * @param simModem 
-     * @param httpClient 
-     * @param endPoint 
-     * @param requestBody 
-     * @return std::string 
-     * @throws HTTPS_NO_GPRS_CONN == 0
-     * @throws HTTPS_NONE_200_RESP == 1
-     */
-    string print(ModemDriver &simModem, HttpClientDriver &httpClient, Headers &headers, const char * endPoint, string requestBody) {
-        if (simModem.isGprsConnected()) { 
-            string response = "";
-            httpClient.setHttpResponseTimeout(HTTPS::_httpsTimeout);
-            httpClient.connectionKeepAlive();
-            httpClient.beginRequest();
-            httpClient.post(endPoint);
-            this->sendHeaders<HttpClientDriver>(httpClient, headers);
-            httpClient.endRequest();
-            httpClient.write((uint8_t*)requestBody.c_str(), requestBody.length());
-            int statusCode = httpClient.responseStatusCode();
-            log_i("Status code: %i", statusCode);
-            if (this->responseOK(statusCode)) {
-                unsigned long timeoutStart = millis();
-                char c;
-                while (
-                    (httpClient.connected() || httpClient.available()) &&
-                    (!httpClient.endOfBodyReached()) &&
-                    ((millis() - timeoutStart) < this->_httpsTimeout)
-                ) {
-                    c = httpClient.read();
-                    response += c;
-                }
+  /**
+   * @brief A simple API for sending a manually contructed request.
+   * 
+   * @param simModem 
+   * @param httpClient 
+   * @param endPoint 
+   * @param requestBody 
+   * @return std::string 
+   * @throws HTTPS_NO_GPRS_CONN == 0
+   * @throws HTTPS_NONE_200_RESP == 1
+   */
+  string print(ModemDriver &simModem, HttpClientDriver &httpClient, Headers &headers, const char * endPoint, string requestBody) {
+    if (simModem.isGprsConnected()) { 
+      string response = "";
+      httpClient.setHttpResponseTimeout(HTTPS::_httpsTimeout);
+      httpClient.connectionKeepAlive();
+      httpClient.beginRequest();
+      httpClient.post(endPoint);
+      this->sendHeaders<HttpClientDriver>(httpClient, headers);
+      httpClient.endRequest();
+      httpClient.write((uint8_t*)requestBody.c_str(), requestBody.length());
+      int statusCode = httpClient.responseStatusCode();
+      log_d("Status code: %i", statusCode);
 
-                return response;
-            } else {
-                log_e("Throwing None 200 response");
-                throw 1;
-            }
-        } else { 
-            log_e("Throwing GPRS disconnected");  
-            throw 0;
-        }    
-    }
+      if (this->responseOK(statusCode)) {
+        unsigned long timeoutStart = millis();
+        char c;
 
-    /**
-     * @brief Download a file using GET and save to the SPIFFS file system,
-     * optionally the user may verify a known CRC32 hash of the file.
-     * 
-     * @param simModem 
-     * @param httpClient 
-     * @param resource 
-     * @param filePath 
-     * @param knownCRC32 
-     * @return true 
-     * @return false 
-     * @throws HTTPS_NONE_200_RESP
-     * @throws HTTPS_NO_GPRS_CONN
-     */
-    template<typename SPIFFSType>
-    bool download(
-        ModemDriver &simModem, 
-        HttpClientDriver &httpClient, 
-        SPIFFSType &Spiffs,
-        const char * resource, 
-        const char * filePath
-    ) {
-        if (simModem.isGprsConnected()) {
-            if (httpClient.connected()) {
-                log_i("Attempting to download");
-                httpClient.get(resource);
-                int statusCode = httpClient.responseStatusCode();
-                log_i("Status code: %i", statusCode);
-                if (this->responseOK(statusCode)) {
+        while (
+          (httpClient.connected() || httpClient.available()) &&
+          (!httpClient.endOfBodyReached()) &&
+          ((millis() - timeoutStart) < this->_httpsTimeout)
+        ) {
+          c = httpClient.read();
+          response += c;
+        }
+
+        return response;
+      } else {
+        log_w("Throwing None 200 response");
+        throw 1;
+      }
+    } else { 
+      log_w("Throwing GPRS disconnected");  
+      throw 0;
+    }    
+  }
+
+  /**
+   * @brief Download a file using GET and save to the SPIFFS file system,
+   * optionally the user may verify a known CRC32 hash of the file.
+   * 
+   * @param simModem 
+   * @param httpClient 
+   * @param resource 
+   * @param filePath 
+   * @param knownCRC32 
+   * @return true 
+   * @return false 
+   * @throws HTTPS_NONE_200_RESP
+   * @throws HTTPS_NO_GPRS_CONN
+   */
+  template<typename SPIFFSType>
+  bool download(
+      ModemDriver &simModem, 
+      HttpClientDriver &httpClient, 
+      SPIFFSType &Spiffs,
+      const char * resource, 
+      const char * filePath
+  ) {
+    if (simModem.isGprsConnected()) {
+      log_d("Attempting to download");
+      httpClient.get(resource);
+      int statusCode = httpClient.responseStatusCode();
+      log_d("Status code: %i", statusCode);
+
+      if (this->responseOK(statusCode)) {
                     
-                    if (Spiffs.exists(filePath)) {
-                        Spiffs.remove(filePath);
-                        log_i("Removed old %s", filePath);
-                    }
-
-                    int contentLength = httpClient.contentLength();
-                    log_i("Content length: %i", contentLength);
-                    int bin;
-                    uint8_t wbuf[HTTPS_WRITE_BUFFER];
-                    uint32_t readLength = 0;
-                    File file = SPIFFS.open(filePath, "a");
-                    unsigned long timeoutStart = millis();
-                    while (
-                        (httpClient.connected() || httpClient.available()) &&
-                        (!httpClient.endOfBodyReached()) 
-                        && ((millis() - timeoutStart) < this->_httpsTimeout)
-                    ) {
-                        if (httpClient.available()) {
-                            bin = httpClient.readBytes(wbuf, sizeof(wbuf));
-                            readLength += file.write(wbuf, bin);
-                            timeoutStart = millis();
-                        } else {
-                            vTaskDelay(50);
-                        }
-                    }
-                    log_i(
-                        "%d bytes downloaded and written to file : %s", 
-                        readLength, 
-                        file.name()
-                    );
-
-                    file.close();
-
-                    log_i("Actually read:  %u", readLength);
-                    log_i("Duration:       %us", millis() - timeoutStart);
-                    log_i("Wait for 3 seconds");
-                    vTaskDelay(3000);
-
-                    if (contentLength == -1) {
-                        return true;
-                    } else if(readLength == contentLength) {
-                        return true;
-                    }
-                } else {
-                    log_e("Modem not connected in https download");
-                }
-            } else {
-                log_e("HTTP client not connected in httpsdownload");
-            }
-        } else {
-            log_e("None 200 response in https download");
+        if (Spiffs.exists(filePath)) {
+          Spiffs.remove(filePath);
+          log_d("Removed old %s", filePath);
         }
-        return false;
-    }
 
-    /**
-     * @brief Read the HTTP response headers to the output stream,
-     * errors if times out or 
-     * 
-     * @param httpClient 
-     */
-    template<class HttpDriver>
-    void readHeaders(HttpDriver &httpClient) {
-        string headerName, headerValue;
-        try {
-            unsigned long headerTimeout = millis();
-            while (httpClient.headerAvailable() && millis() - headerTimeout < this->_httpsTimeout) {
-                headerName = httpClient.readHeaderName().c_str();
-                headerValue = httpClient.readHeaderValue().c_str();
-                log_i("%s : %s", headerName.c_str(), headerValue.c_str());
-            }
-            int length = httpClient.contentLength();
-            if (length >= 0) {
-                log_i("Content length: %i", length);
-            }
-        } catch(uint8_t error) {
-            throw 2;
-            log_e("failed when reading response headers, last header read: '%s': '%s'", headerName.c_str(), headerValue.c_str());
-        }
-    }
+        int contentLength = httpClient.contentLength();
+        log_d("Content length: %i", contentLength);
+        int bin;
+        uint8_t wbuf[HTTPS_WRITE_BUFFER];
+        uint32_t readLength = 0;
+        File file = SPIFFS.open(filePath, "a");
+        unsigned long timeoutStart = millis();
 
-    /**
-     * @brief Verifiy the response code is in the accepted group of codes
-     * 
-     * @param statusCode 
-     * @return true 
-     * @return false 
-     */
-    bool responseOK(int statusCode) {
-        switch (statusCode) {
-            case HTTPS_OK_RESPONSE_CODE:
-            case HTTPS_ACCEPTED_RESPONSE_CODE:
-            case HTTPS_CREATED_RESPONSE_CODE: return true; break;
-            default: return false;
+        while (
+          (httpClient.connected() || httpClient.available()) &&
+          (!httpClient.endOfBodyReached()) 
+          && ((millis() - timeoutStart) < this->_httpsTimeout)
+        ) {
+          if (httpClient.available()) {
+            bin = httpClient.readBytes(wbuf, sizeof(wbuf));
+            readLength += file.write(wbuf, bin);
+            timeoutStart = millis();
+          } else {
+            vTaskDelay(50);
+          }
         }
-    }
 
-    /**
-     * @brief A simple mechanism to send a group of HTTP headers.
-     * 
-     * @tparam HttpDriver 
-     * @param httpClient 
-     * @param headers 
-     * @return true 
-     * @return false 
-     */
-    template<class HttpDriver>
-    void sendHeaders(HttpDriver &httpClient, Headers headers) {
-        for (int i = 0; i < headers.size(); ++i) {
-            httpClient.sendHeader(headers[i].first, headers[i].second);
+        log_d("%d bytes downloaded and written to file : %s", readLength, file.name());
+        file.close();
+        log_d("Actually read:  %u", readLength);
+        log_d("Duration:       %us", millis() - timeoutStart);
+        log_d("Wait for 3 seconds");
+        vTaskDelay(3000);
+
+        if (contentLength == -1) {
+          return true;
+        } else if(readLength == contentLength) {
+          return true;
         }
+
+      } else {
+        log_w("None 200 response in https download");
+      }
+    } else {
+      log_w("Modem not connected in https download");
     }
+    return false;
+  }
+
+  /**
+   * @brief Read the HTTP response headers to the output stream,
+   * errors if times out or 
+   * 
+   * @param httpClient
+   * @throws 2
+   * @return void 
+   */
+  template<class HttpDriver>
+  void readHeaders(HttpDriver &httpClient) {
+    string headerName, headerValue;
+    try {
+      unsigned long headerTimeout = millis();
+      while (httpClient.headerAvailable() && millis() - headerTimeout < this->_httpsTimeout) {
+        headerName = httpClient.readHeaderName().c_str();
+        headerValue = httpClient.readHeaderValue().c_str();
+        log_d("%s : %s", headerName.c_str(), headerValue.c_str());
+      }
+      int length = httpClient.contentLength();
+
+      if (length >= 0) {
+        log_d("Content length: %i", length);
+      }
+    } catch(uint8_t error) {
+      throw 2;
+      log_e("failed when reading response headers, last header read: '%s': '%s'", headerName.c_str(), headerValue.c_str());
+    }
+  }
+
+  /**
+   * @brief Verifiy the response code is in the accepted group of codes
+   * 
+   * @param statusCode 
+   * @return true 
+   * @return false 
+   */
+  bool responseOK(int statusCode) {
+    switch (statusCode) {
+      case HTTPS_OK_RESPONSE_CODE:
+      case HTTPS_ACCEPTED_RESPONSE_CODE:
+      case HTTPS_CREATED_RESPONSE_CODE: return true; break;
+      default: return false;
+    }
+  }
+
+  /**
+   * @brief A simple mechanism to send a group of HTTP headers.
+   * 
+   * @tparam HttpDriver 
+   * @param httpClient 
+   * @param headers 
+   * @return true 
+   * @return false 
+   */
+  template<class HttpDriver>
+  void sendHeaders(HttpDriver &httpClient, Headers headers) {
+    for (int i = 0; i < headers.size(); ++i) {
+      httpClient.sendHeader(headers[i].first, headers[i].second);
+    }
+  }
 };
